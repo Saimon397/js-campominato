79fc4fc2549fbbd95070e6b8cce931bef0c313ee
Saimon397 <simonecarrabba@gmail.com>
2022-10-17 18:10:02 +0200

    Add_js_e_mod_style

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const btnPlay = document.getElementById('play');
+
+function play() {
+    console.log('Start Game...');
+    const NUM_BOMB = 16;
+    const bombsPosition = [];
+    let numCell;
+    const fieldGame = document.getElementById('field-game');
+    fieldGame.innerHTML = '';
+    const levelHTML = document.getElementById('difficulty');
+    const level = levelHTML.value;
+    switch(level) {
+        case '1':
+        default:
+            numCell = 100;
+            break;
+        case '2':
+            numCell = 81;
+            break;
+        case '3':
+            numCell = 49;
+            break;
+    }
+
+
+    // funzione che genera la cella
+    function drawCell(num) {
+        const cellPerSide = Math.sqrt(numCell);
+        const cell = document.createElement('div');
+        cell.className = 'square';
+        cell.style.width = `calc(100% / ${cellPerSide})`;
+        cell.style.height = `calc(100% / ${cellPerSide})`;
+        cell.innerHTML = `
+            <span>${num}</span>
+        `;
+        cell.addEventListener('click', function() {
+            this.classList.add('bg-game');
+        });
+        return cell;
+    }
+    
+    while(bombsPosition.length < NUM_BOMB) {
+        const bomb = randomNumber(1, numCell);
+        if(!bombsPosition.includes(bomb)) {
+            bombsPosition.push(bomb);
+        }
+    }
+    console.log(bombsPosition);
+
+    // funzione che genera il campo di gioco
+    function drawGrid() {
+        const grid = document.createElement('div');
+        grid.className = 'grid';
+        for(let i = 1; i <= numCell; i++) {
+            const cell = drawCell(i);
+            grid.appendChild(cell);
+        }
+        fieldGame.appendChild(grid);
+    }
+    // chiamo la funzione
+    drawGrid();
+}
+// attacco event listener al bottone play
+btnPlay.addEventListener('click', play);
